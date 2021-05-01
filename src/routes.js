8d089cde92bb5acc7ad4e41e57b5d8d358466d3c
Marcos Moraes Pereira <marcosmpereira1992@gmail.com>
2021-05-01 20:31:43 -0300

    [UPDATE]- Adicionado rota de exclusão de usuário

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,11 @@ const HistoricoController = require('./controllers/HistoricoController');
 
 const routes = express.Router();
 
-routes.get('/user/:userId', UserController.getUsuarioById);
 routes.get('/user', UserController.getUsuarios);
+routes.get('/user/:userId', UserController.getUsuarioById);
 routes.post('/user/:userId/alterar', UserController.updateUsuario);
 routes.post('/user', UserController.createUsuario);
+routes.post('/user/:userId/excluir', UserController.deleteUsuario);
 
 routes.get('/historico/:historicoId', HistoricoController.getHistoricoById);
 routes.post('/historico', HistoricoController.createHistorico);
