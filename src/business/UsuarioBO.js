26a36eb0625eb7260ebec8abb9ca347ff6d7871b
Marcos Moraes Pereira <marcosmpereira1992@gmail.com>
2021-05-01 19:53:43 -0300

    [UPDATE] - Ajuste no método de criação de usuário para suportar alteração

--- src/business/UsuarioBO.js
+++ src/business/UsuarioBO.js
@@ -1,13 +1,32 @@
+
 const Usuario = require('../models/Usuario');
 const Utils = require('../Utils');
 
 const UsuarioBO = {
 
-  async createUsuario(usuarioRequest) {
+  async createUsuario(usuarioRequest, idUsuario = null) {
     if(usuarioRequest != undefined) {
       const isCamposValidos = this.validaCamposCriarUsuario(usuarioRequest);
       if(isCamposValidos.isValid === true) {
-        return await Usuario.createUsuario(usuarioRequest); 
+        
+        if(idUsuario !== null) {
+          const usuario = await Usuario.getUsuarioById(idUsuario);
+          if(usuario && usuario.id){
+            usuarioRequest.id = usuario.id;
+            return {
+              data: await Usuario.updateUsuario(usuarioRequest),
+              msg: Utils.getMessage("Usuário alterado com sucesso!").msg
+            }; 
+          } else { 
+            return Utils.getMessage("Usuário inexistente!");
+          }
+        } else { 
+          return await {
+            data: Usuario.createUsuario(usuarioRequest),
+            msg: Utils.getMessage("Usuário cadastrado com sucesso!").msg
+          }; 
+        }
+
       }
       return Utils.getMessage(isCamposValidos.msg);
     }
@@ -15,7 +34,10 @@ const UsuarioBO = {
   },
 
   async getUsuarioById(id) {
-    return await Usuario.getUsuarioById(id);
+    if(id && id !== null && id !== undefined) {
+      return await Usuario.getUsuarioById(id);
+    }
+    return Utils.getMessage("Campo id não informado!");
   },
 
   validaCamposCriarUsuario(campos) {
